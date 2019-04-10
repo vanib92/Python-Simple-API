60cb25b4eb8774d3e7746ddc625f159171a3f1ae
vanib92 <49475032+vanib92@users.noreply.github.com>
2019-04-10 15:29:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apidb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-db.create_all()
 db = SQLAlchemy(app)
 
 class User(db.Model):
